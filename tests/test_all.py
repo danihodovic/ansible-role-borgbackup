50e6c4a99c16e3d6747a85b0aa3eb0a89e0479c7
Dani Hodovic <dani.hodovic@gmail.com>
2021-04-17 19:26:42 +0300

    Reformat tests

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,11 +1,14 @@
 def test_borg_binary(host):
     assert host.exists("borg")
 
+
 def test_borgmatic_binary(host):
     assert host.exists("borgmatic")
 
+
 def test_config_file(host):
     assert host.file("/etc/borgmatic/config.yaml").exists
+
 
 def test_run_borgmatic(host):
     result = host.run("borgmatic")
